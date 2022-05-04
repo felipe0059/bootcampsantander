34895b5d359598ef9fb2b3e29c9af88735cfd178
felipe0059 <felipe.nx4@gmail.com>
2022-05-03 21:36:16 -0300

    primeiro commit do projeto

---
+++ src/main/java/br/com/santander/ApibancoApplication.java
@@ -0,0 +1,13 @@
+package br.com.santander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApibancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApibancoApplication.class, args);
+	}
+
+}
